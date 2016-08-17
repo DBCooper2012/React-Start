ce6785fc2b54a68bfa41f894639a4505f682f3c8
Barry Paterson <barry.paterson@alcidion.com>
2016-08-17 11:56:58 +0930

    React Project Start

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import "babel-polyfill"
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import StartComponent from './components/StartComponent'
+
+ReactDOM.render(
+  <StartComponent />,
+  document.getElementById('root')
+);
